7515866ad9ee145d541c57204e4f9ca9d666ff15
soyaibur <ovisoyaiburrahman@gmail.com>
2021-08-09 22:10:09 +0600

    You may write inline style within render method with in jsx code. Here You have to take to curly brases. one for writing style and another for as an object

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const heading = "Todo Website";
+const cardHeading = "Call my family";
+const cardTaxt = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ratione nemo expedita assumenda accusantium nostrum sunt ad earum sit doloremque.";
+const date = new Date();
+const todayDate = date.getUTCDate();
+const currentMonth = date.getUTCMonth();
+const currentYear = date.getUTCFullYear();
 
 
 ReactDOM.render(
-   <h1>Hello World. </h1>,
+   <div>
+     <h1 style={{color:'white',backgroundColor:'purple'}}>{heading}</h1>
+     <div>
+       <h3>{cardHeading}</h3>
+       <p>{cardTaxt}</p>
+       <p>{todayDate + "/" + currentMonth + "/" + currentYear}</p>
+     </div>
+   </div>,
   
    document.getElementById('root'),
 );
-
