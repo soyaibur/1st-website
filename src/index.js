218ff3bf57015da9411d66c839889cf8d95e3b59
soyaibur <ovisoyaiburrahman@gmail.com>
2021-08-09 22:24:38 +0600

    this is how you may add in line css in jsx or in render method

--- src/index.js
+++ src/index.js
@@ -9,11 +9,19 @@ const todayDate = date.getUTCDate();
 const currentMonth = date.getUTCMonth();
 const currentYear = date.getUTCFullYear();
 
+const headingStyle = {
+  color: "white",
+  textAlign: "center",
+  padding: "3rem",
+  backgroundColor: "purple",
+  margin: "0"
+}
+
 
 ReactDOM.render(
    <div>
-     <h1 style={{color:'white',backgroundColor:'purple'}}>{heading}</h1>
-     <div>
+     <h1 style={headingStyle}>{heading}</h1>
+     <div style={{backgroundColor:'wheat',width:'30vw',display:'flex',textAlign:'center',justifyContent:'center',flexDirection:'column',padding:'1rem'}}>
        <h3>{cardHeading}</h3>
        <p>{cardTaxt}</p>
        <p>{todayDate + "/" + currentMonth + "/" + currentYear}</p>
