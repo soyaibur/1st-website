38ea0a61a72ca6fa8ec31b709057214b854a4ad5
soyaibur <ovisoyaiburrahman@gmail.com>
2021-08-10 08:13:16 +0600

    Props and distructuring .props means in component there would be no actual text within tag. Our text will be from app component where card component to be call out. text would be written in an attribute like href='';

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ const heading = "Todo Website";
 function App(){
    return <div>
        <h1 className="headingStyle">{heading}</h1>,
-       <Card />
-       <Card />
-       <Card />
+       <Card titleTaxt="call Mother" descrip="this is description text" />
+       <Card  titleTaxt="call Father" descrip="this is Massage for nothing" />
+       <Card  titleTaxt="call Papo" descrip="this is a papo call" />
    </div>
 }
 
--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
-const cardHeading = "Call my family";
-const cardTaxt = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ratione nemo expedita assumenda accusantium nostrum sunt ad earum sit doloremque.";
 const date = new Date();
 const todayDate = date.getUTCDate();
 const currentMonth = date.getUTCMonth();
 const currentYear = date.getUTCFullYear();
 
-function Card(){
+function Card(props){
+    const {titleTaxt,descrip} = props;
     return <div className="card">
-              <h3>{cardHeading}</h3>
-              <p>{cardTaxt}</p>
+              <h3>{props.titleTaxt}</h3>
+              <p>{descrip}</p>
               <p>{todayDate + "/" + currentMonth + "/" + currentYear}</p>
           </div>
             
